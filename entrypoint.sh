79ffaf7177f7413864a2806c268081aa11842481
Georg Pitterle <1034074+georg-pitterle@users.noreply.github.com>
2026-03-15 22:51:06 +0100

    Fixe Docker install10

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Wait for database to be ready
-until mysqladmin ping -h ${DB_HOST} -u ${DB_USERNAME} -p${DB_PASSWORD} --ssl-mode=DISABLED --silent; do
+until mysqladmin ping -h ${DB_HOST} -u ${DB_USERNAME} -p${DB_PASSWORD} --ssl=0 --silent; do
   echo "Waiting for database..."
   sleep 2
 done
