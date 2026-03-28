f8e467f30c72929b2d12125a1ffc8f6585976472
Georg Pitterle <1034074+georg-pitterle@users.noreply.github.com>
2026-03-28 23:07:54 +0100

    fix: render navbar toggler with bootstrap icon for reliable visibility

--- tests/Feature/LayoutFeatureTest.php
+++ tests/Feature/LayoutFeatureTest.php
@@ -16,6 +16,7 @@ class LayoutFeatureTest extends TestCase
         $this->assertIsString($layoutContent);
         $this->assertStringContainsString('class="app-topbar__brand-name"', $layoutContent);
         $this->assertStringContainsString('class="navbar-toggler"', $layoutContent);
+        $this->assertStringContainsString('class="bi bi-list fs-3 text-white"', $layoutContent);
         $this->assertStringContainsString('navbar-expand-lg', $layoutContent);
     }
 
@@ -30,14 +31,13 @@ class LayoutFeatureTest extends TestCase
         $this->assertStringContainsString('display: none;', $styleContent);
     }
 
-    public function testTopbarCssDefinesExplicitVisibleTogglerIcon(): void
+    public function testTopbarCssDefinesVisibleTogglerIconStyling(): void
     {
         $stylePath = dirname(__DIR__) . '/../public/css/style.css';
         $styleContent = file_get_contents($stylePath);
 
         $this->assertIsString($styleContent);
-        $this->assertStringContainsString('.navbar.bg-dark.app-topbar .navbar-toggler-icon', $styleContent);
-        $this->assertStringContainsString('background-image: url("data:image/svg+xml', $styleContent);
-        $this->assertStringContainsString('rgba(255,255,255,0.95)', $styleContent);
+        $this->assertStringContainsString('.navbar.bg-dark.app-topbar .navbar-toggler .bi-list', $styleContent);
+        $this->assertStringContainsString('display: block;', $styleContent);
     }
 }
